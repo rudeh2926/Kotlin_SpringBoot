fe410130807f5ac173e11c8801368e87930a94c7
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-18 22:33:43 +0900

    수정

--- Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/global/error/GlobalExceptionFilter.kt
+++ Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/global/error/GlobalExceptionFilter.kt
@@ -1,5 +1,6 @@
 import com.example.kotlin_springboot.global.error.ErrorResponse
 import com.example.kotlin_springboot.global.error.exception.BusinessException
+import com.example.kotlin_springboot.global.error.exception.ErrorCode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
@@ -9,32 +10,23 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class GlobalExceptionFilter(
-    private val objectMapper: ObjectMapper
-) : OncePerRequestFilter() {
-
+    private val objectMapper: ObjectMapper) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
-        filterChain: FilterChain
-    ) {
+        filterChain: FilterChain) {
         try {
             filterChain.doFilter(request, response)
         } catch (e: BusinessException) {
             val errorCode = e.errorCode
             val errorResponse = ErrorResponse(errorCode.status, errorCode.message, LocalDateTime.now())
             writeErrorResponse(response, errorCode.status, errorResponse)
-        } catch (e: Exception) {
-            val errorResponse = ErrorResponse(
-                HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Internal Server Error",
-                LocalDateTime.now())
-            writeErrorResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, errorResponse)
         }
     }
 
     @Throws(IOException::class)
-    private fun writeErrorResponse(response: HttpServletResponse, status:Int, errorResponse:
-    ErrorResponse
-    ) {
+    private fun writeErrorResponse(response: HttpServletResponse,status:Int ,errorResponse:
+    ErrorResponse) {
         response.status = status
         response.contentType = "application/json"
         response.characterEncoding = "UTF-8"
