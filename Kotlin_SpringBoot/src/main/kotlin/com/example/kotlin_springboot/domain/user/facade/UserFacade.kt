96a5139cbab904c629e3b0386900f7f7f968bdc8
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-19 11:57:42 +0900

    facade 작성

---
+++ Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/domain/user/facade/UserFacade.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_springboot.domain.user.facade
+
+import com.example.kotlin_springboot.domain.user.domain.User
+import com.example.kotlin_springboot.domain.user.domain.repository.UserRepository
+import com.example.kotlin_springboot.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+   private val userRepository: UserRepository
+) {
+    fun getCurrentUser(): User? {
+        val email : String = SecurityContextHolder.getContext().authentication.name
+        return getUserEmail(email)
+    }
+
+    fun getUserEmail(email : String) : User?{
+        return userRepository.findByEmail(email).orElseThrow{UserNotFoundException}
+    }
+}
