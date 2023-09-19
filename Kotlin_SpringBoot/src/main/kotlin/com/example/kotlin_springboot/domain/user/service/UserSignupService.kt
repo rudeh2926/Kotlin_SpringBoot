1b2007b6945d17c5d3f80a547ec4312fc04e4db4
도경 <dokyungkim2926@gmail.com>
2023-09-19 22:17:05 +0900

    회원가입

--- Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/domain/user/service/UserSignupService.kt
+++ Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/domain/user/service/UserSignupService.kt
@@ -2,26 +2,30 @@ package com.example.kotlin_springboot.domain.user.service
 
 import com.example.kotlin_springboot.domain.user.domain.User
 import com.example.kotlin_springboot.domain.user.domain.repository.UserRepository
+import com.example.kotlin_springboot.domain.user.exception.EmailAlreadyExistsException
 import com.example.kotlin_springboot.domain.user.presentation.dto.request.UserSignupRequest
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.UUID
 import javax.transaction.Transactional
 
 @Service
 class UserSignupService (
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
     @Transactional
     fun signup(userSignupRequest: UserSignupRequest) {
 
-        if (userRepository.existsByEmail(userSignupRequest.email)) {}
+        if (userRepository.existsByEmail(userSignupRequest.email)) {
+            throw EmailAlreadyExistsException
+        }
 
         userRepository.save(
             User(
                 id = UUID(0, 0),
-                password = userSignupRequest.password,
+                password = passwordEncoder.encode(userSignupRequest.password),
                 email = userSignupRequest.email
-
             )
         )
     }
