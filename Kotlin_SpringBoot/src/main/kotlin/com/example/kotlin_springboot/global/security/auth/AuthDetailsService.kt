9afc70cacb52c8d0752fb15720f2d20e97830123
rudeh2926 <dokyungkim2926@gmail.com>
2023-09-19 12:09:35 +0900

    추가

---
+++ Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_springboot.global.security.auth
+
+import com.example.kotlin_springboot.domain.user.domain.User
+import com.example.kotlin_springboot.domain.user.facade.UserFacade
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+class AuthDetailsService (
+    private val userFacade: UserFacade
+) : UserDetailsService {
+    override fun loadUserByUsername(email : String) : UserDetails {
+         val user : User? = email.let { userFacade.getUserEmail(it)}
+        return AuthDetails(user!!.email)
+    }
+}
