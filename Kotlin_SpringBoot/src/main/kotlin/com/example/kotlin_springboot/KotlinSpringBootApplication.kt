914358f3b2908e626127cb15378f6bf9e72ee554
도경 <dokyungkim2926@gmail.com>
2023-09-19 18:55:35 +0900

    의존성

--- Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/KotlinSpringBootApplication.kt
+++ Kotlin_SpringBoot/src/main/kotlin/com/example/kotlin_springboot/KotlinSpringBootApplication.kt
@@ -1,9 +1,11 @@
 package com.example.kotlin_springboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class KotlinSpringBootApplication
 
 fun main(args: Array<String>) {
